b26e0252dd96be6f207aa760ac6283d000342569
jesse-hampton-bu <65149406+jesse-hampton-bu@users.noreply.github.com>
2020-09-05 21:05:26 -0700

    third-commit

--- week2/basic_website_wJS.js
+++ week2/basic_website_wJS.js
@@ -1,24 +1,36 @@
-var h1 = document.createElement("h1")
+// let nav = document.createElement("ol")
+// document.body.append(ol)
+
+// let  li4 = document.createElement("li")
+// li.textContent = "link 1"
+
+// let  li5 = document.createElement("li")
+// li2.textContent = "link 2"
+// ol.append(li4, li5)
+
+
+
+let h1 = document.createElement("h1")
 document.body.append(h1)
 h1.textContent = "I hope I am doing this right"
 
-var p = document.createElement("p")
+let p = document.createElement("p")
 document.body.append(p)
 p.textContent = "if not then i need help"
 
-var ol = document.createElement("ol")
+let ol = document.createElement("ol")
 document.body.append(ol)
 
-var  li = document.createElement("li")
+let  li = document.createElement("li")
 li.textContent = "thing .1"
 
-var  li2 = document.createElement("li")
+let  li2 = document.createElement("li")
 li2.textContent = "thing .2"
 
-var  li3 = document.createElement("li")
+let  li3 = document.createElement("li")
 li3.textContent = "thing .3"
 ol.append(li, li2, li3)
 
-var footer = document.createElement("footer")
+let footer = document.createElement("footer")
 document.body.append(footer)
 footer.textContent = "I am finally done"
--- week2/creating&Appending_elements.js
+++ week2/creating&Appending_elements.js
@@ -1,20 +1,20 @@
-var h1 = document.createElement("h1")
+let h1 = document.createElement("h1")
 document.body.append(h1)
 h1.textContent = "Welcome to my JS site"
 
-var p = document.createElement("p")
+let p = document.createElement("p")
 document.body.append(p)
 p.textContent = "All of this was created by JavaScript"
 
-var ol = document.createElement("ol")
+let ol = document.createElement("ol")
 document.body.append(ol)
 
-var  li = document.createElement("li")
+let  li = document.createElement("li")
 li.textContent = "Item 1"
 
-var  li2 = document.createElement("li")
+let  li2 = document.createElement("li")
 li2.textContent = "Item 2"
 
-var  li3 = document.createElement("li")
+let  li3 = document.createElement("li")
 li3.textContent = "Item 3"
 ol.append(li, li2, li3)
