63100473c9687f284c8fd8c61589fe1103228bb4
jesse-hampton-bu <65149406+jesse-hampton-bu@users.noreply.github.com>
2020-10-08 16:06:55 -0700

    ninth-commit

--- week5/plane.js
+++ week5/plane.js
@@ -10,11 +10,3 @@ alert(" First Name: ".concat(fname.value).concat("\n")
 
 
 
-myFunction = () => (fname.value, lname.value, age.value !== "") ? 
-alert(" First Name: " + "" + (fname.value) + "" + ("\n")
- + "" + (" Last Name: ") + "" + (lname.value) + "" + ("\n")
- + "" + (" Age: ") + "" + (age.value) + "" + ("\n")
- + "" + (" Gender: ") + "" + (gender.value) + "" + ("\n")
- + "" + (" Destination: ") + "" + (city.value) + "" + ("\n")
- + "" + (" Dietary Restrictions: ") + "" + (dietaryrestrictions.value)) :
-  alert(" Please Fill Out The Required Fields ")
